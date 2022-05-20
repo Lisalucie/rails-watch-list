a95b1eabb1235ee3e0a76b69c4af7bc3ba82a123
lisa loreau <lisalor44@gmail.com>
2022-05-20 11:40:05 +0200

    install heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #   resources :movie, only: [:show]
+  root to: 'lists#index'
+  resources :lists, only: [:index, :show, :create, :new] do
+    resources :bookmarks, only: [:destroy, :create, :new]
+  end
 end
